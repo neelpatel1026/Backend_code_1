7aa497081aed1508a54a29e3eaa995caa47b1be0
neelpatel1026 <neelpatel8422@gmail.com>
2025-11-01 16:58:44 +0530

    discuss about utils files

--- src/db/index.js
+++ src/db/index.js
@@ -13,20 +13,4 @@ const connectDB = async () => {
 
 export default connectDB
 
-// import mongoose from "mongoose";
-// import { DB_NAME } from "../constants.js";
 
-// const connectDB = async () => {
-//     try {
-//         const connectionInstance = await mongoose.connect(
-//             `${process.env.MONGODB_URI}/${DB_NAME}?retryWrites=true&w=majority&ssl=true&authSource=admin`
-//         );
-//         console.log(`✅ MongoDB Connected: ${connectionInstance.connection.host}`);
-//     } catch (error) {
-//         console.log("❌ MONGODB CONNECTION FAILED");
-//         console.log(error);
-//         process.exit(1);
-//     }
-// };
-
-// export default connectDB;
---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        Errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.Errors = Errors
+
+        if(stack){
+            this.stack = stack
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {ApiError}
